000c8caea0dde194f81c8b2c711b6e3468385173
395143360 <395143360@qq.com>
2025-02-25 13:53:44 +0800

    add hieraDETR init

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -18,10 +18,16 @@ from .ssd_vgg import SSDVGG
 from .swin import SwinTransformer
 from .trident_resnet import TridentResNet
 
+############################
+from .hierabackbone import HieraBackbone
+
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet',
     'MobileNetV2', 'Res2Net', 'HourglassNet', 'DetectoRS_ResNet',
     'DetectoRS_ResNeXt', 'Darknet', 'ResNeSt', 'TridentResNet', 'CSPDarknet',
     'SwinTransformer', 'PyramidVisionTransformer',
-    'PyramidVisionTransformerV2', 'EfficientNet', 'CSPNeXt'
+    'PyramidVisionTransformerV2', 'EfficientNet', 'CSPNeXt',
+
+    "HieraBackbone"
 ]
+
